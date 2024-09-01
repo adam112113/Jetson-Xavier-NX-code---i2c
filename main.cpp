616beeab52f0cf3a11b972c024892282db8e2d45
adam112113 <115705424+adam112113@users.noreply.github.com>
2024-09-01 23:14:10 +0200

    Update main.cpp
    yuftufuftu

--- main.cpp
+++ main.cpp
@@ -24,19 +24,22 @@ void turnOnLED(PCF8574AController& controller, int ledIndex, int state);
     //controller3.initialize();
     //controller4.initialize();
 int main() {
+    int precentage = 50;
 
+    input_precentage(percentage);
+    /*
     for(int i = 0; i <= 100; i++)
     {
         input_percentage(i);
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        //std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
-    
+    */
     return 0;
 }
 
 void turnOnLED(PCF8574AController& controller, int ledIndex, int state) {
     // The ledIndex parameter corresponds to the pin on the PCF8574A (0-3 for each controller)
-    // The state parameter is either 1 (turn LED on) or 0 (turn LED off)
+    // The state parameter is either 0 (turn LED on) or 1 (turn LED off)
 
     // Call the setLedState method on the controller object
     controller.setLedState(ledIndex, state);
@@ -49,7 +52,7 @@ void input_percentage(int percentage) {
 
     // Loop through all LEDs and turn them on based on the calculated number
     for (int i = 0; i < totalLEDs; i++) {
-        int ledState = (i < ledsOn) ? 1 : 0; // Turn on LEDs if within the range, else turn off
+        int ledState = (i < ledsOn) ? 0 : 1; // Turn on LEDs if within the range, else turn off
         //int ledState;
         //if (i < ledsOn) {
         //ledState = 1;  // Turn on the LED
@@ -59,16 +62,16 @@ void input_percentage(int percentage) {
 
         if (i < 4) {
             // First LED strip (controller1)
-            turnOnLED(controller1, i, 1);
+            turnOnLED(controller1, i, 0);
         } else if (i < 8) {
             // Second LED strip (controller2)
-            turnOnLED(controller2, i - 4, 1);
+            turnOnLED(controller2, i - 4, 0);
         } else if (i < 12) {
             // Third LED strip (controller3)
-            turnOnLED(controller3, i - 8, 1);
+            turnOnLED(controller3, i - 8, 0);
         } else if (i < 16) {
             // Fourth LED strip (controller4)
-            turnOnLED(controller4, i - 12, 1);
+            turnOnLED(controller4, i - 12, 0);
         }
     }
 }
