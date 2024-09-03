7f4aba053f769a70a361d957527a2d32be2409ad
adam112113 <115705424+adam112113@users.noreply.github.com>
2024-09-03 22:54:35 +0200

    Update main.cpp
    Final changes

--- main.cpp
+++ main.cpp
@@ -3,20 +3,14 @@
 #include <thread>
 #include <chrono>
 
-//void turnOnLed(PCF8574AController& controller, int numLed) {
-//   for (int i = 0; i < numLeds; ++i) {
-//        controller.setLedState(numLED, 0);  // Turn on LED
-//        //std::this_thread::sleep_for(std::chrono::milliseconds(500));
-//    }
-//}
 void input_percentage(int percentage);
 void turnOnLED(PCF8574AController& controller, int ledIndex, int state);
 
 // Create an instance of the controller for each setup
-    PCF8574AController controller1(0);
-    PCF8574AController controller2(1);
-    PCF8574AController controller3(2);
-    PCF8574AController controller4(3);
+PCF8574AController controller1(0);
+PCF8574AController controller2(1);
+PCF8574AController controller3(2);
+PCF8574AController controller4(3);
 
 
 int main() {
@@ -27,12 +21,12 @@ int main() {
     controller4.initialize();
     
     //int percentage = 50;
-
     //input_percentage(percentage);
-    ///*
+    
     for(int i = 0; i <= 100; i++)
     {
         input_percentage(i);
+        // Delay
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
     //*/
@@ -40,16 +34,14 @@ int main() {
 }
 
 void turnOnLED(PCF8574AController& controller, int ledIndex, int state) {
-    // The ledIndex parameter corresponds to the pin on the PCF8574A (0-3 for each controller)
-    // The state parameter is either 0 (turn LED on) or 1 (turn LED off)
-
-    // Call the setLedState method on the controller object
+    // The ledIndex corresponds to I2C module (0-3 for each controller)
+    // The state is 0 (turn LED on) or 1 (turn LED off)
     controller.setLedState(ledIndex, state);
 }
 
 
 void input_percentage(int percentage) {
-    int totalLEDs = 16;                       // Total number of LEDs
+    int totalLEDs = 16;                          // Total number of LEDs
     int ledsOn = (percentage * totalLEDs) / 100; // Number of LEDs that should be on
 
     // Loop through all LEDs and turn them on based on the calculated number
